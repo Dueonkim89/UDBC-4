d23f1ff7a74dbae05db641a0fdd9269496bae611
Dueon Kim <dueonkim89@Yahoo.com>
2018-11-06 23:23:57 -0800

    working on post route for /requestValidation. alot more logic to be added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
+//import BlockChain and Block class
 const { Blockchain } = require('./simpleChain');
 const { Block } = require('./simpleBlock');
+
+//import helper functions
+const { createValidationObject } = require('./utils/helper');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -14,7 +18,7 @@ app.get("/block/:height", (req, res) => {
 	const height = req.params.height;
 	newBlockChain.getBlock(height).then( block => {	
 		res.send(block);
-	}).catch(error => res.status(400).send(error));				
+	}).catch(error => res.status(400).send(error));		
 });
 
 app.post("/block", (req,res) => {
@@ -25,6 +29,18 @@ app.post("/block", (req,res) => {
 		newBlockChain.addBlock(new Block(blockData)).then(block => {
 			res.send(block);
 		}).catch(error => res.status(400).send(error));	
+	}
+});
+
+app.post("/requestValidation", (req,res) => {
+	const address = req.body.address;
+	
+	if (!address) {
+		res.status(404).send('Please provide a blockchain identity');
+	} else {
+		// invoke helper function
+		const valObject = createValidationObject(address);
+		res.send(valObject);
 	}
 });
 
