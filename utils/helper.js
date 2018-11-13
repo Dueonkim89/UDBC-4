d7d4bc6478172210fdf91ff27234d71b085d5ef8
Dueon Kim <dueonkim89@Yahoo.com>
2018-11-12 22:10:39 -0800

    create 2 functions to check if valid ASCII and byte size of star story

--- utils/helper.js
+++ utils/helper.js
@@ -81,8 +81,26 @@ function createResponseForValidSig(addy) {
 	return response;
 }
 
+//check if valid ASCII
+function isASCII(str) {
+    return /^[\x00-\x7F]*$/.test(str);
+}
+
+//check if 500 bytes or under
+function checkBytesOfStory(story) {
+	const buf = Buffer.from(story, 'ascii');
+	const length = Buffer.byteLength(buf);
+	if (length > 500) {
+		return false;
+	} else {
+		return buf.toString('hex');
+	}
+}
+
 module.exports = {
 	checkValidation,
 	validateSignature,
-	createResponseForValidSig
+	createResponseForValidSig,
+	isASCII,
+	checkBytesOfStory
 }
