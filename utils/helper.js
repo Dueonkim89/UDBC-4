3cab1750a57cbb4d28017e54f790175619edd1db
Dueon Kim <dueonkim89@Yahoo.com>
2018-11-16 00:04:25 -0800

    working on 30 min validation window for valid requests

--- utils/helper.js
+++ utils/helper.js
@@ -4,6 +4,7 @@ const bitcoinMessage = require('bitcoinjs-message');
 
 //to store the wallets that need to be validated. will contain mutable data
 let memPool = [];
+let validMemPool = [];
 
 //function to check if the validation object exists.
 function checkValidation(address, validated=null) {
@@ -14,10 +15,11 @@ function checkValidation(address, validated=null) {
 	}
 	//if previousRequest exists, find new validation window.
 	if (previousRequest.length) {
-		let valRequest = calculateNewValidationWindow(previousRequest[0]);
-		//filter and push new object.
+		let valRequest = calculateNewValidationWindow(previousRequest[0], 300000);
+		//remove request within mempool that has the address
 		memPool = memPool.filter(request => request.address !== address);
-		memPool.push(valRequest);
+		//filter and push new object.
+		memPool.push(valRequest);			
 		console.log(memPool);
 		//return the valRequest
 		return valRequest;
@@ -28,16 +30,16 @@ function checkValidation(address, validated=null) {
 }
 
 // function to recalculate the validation window
-function calculateNewValidationWindow(request) {
+function calculateNewValidationWindow(request, timeLimit) {
 	//calculate new available validation time.
 	const timeAtRequest = parseInt(request.requestTimeStamp);
-	const expirationTime = timeAtRequest + 300000;
+	const expirationTime = timeAtRequest + timeLimit;
 	//if 5 min validation window is over. create new validation request
 	if (new Date().getTime() >= expirationTime) {
 		return createNewValidationRequest(request.address);
 	// else create new 	validationWindow
 	} else {
-		request.validationWindow = `${(expirationTime - new Date().getTime()) /1000} seconds`;
+		request.validationWindow = `${(expirationTime - new Date().getTime()) /1000}`;
 		return request;
 	}
 }
@@ -45,10 +47,13 @@ function calculateNewValidationWindow(request) {
 // create new validation request.
 function createNewValidationRequest(address) {
 	const requestTimeStamp = new Date().getTime().toString();
-	const validationWindow = '300 seconds';
+	const validationWindow = 300;
 	const message =`${address}:${requestTimeStamp}:starRegistry`;
 	let newRequest = {address, requestTimeStamp, message, validationWindow};
-	memPool.push(newRequest);
+	//remove request within mempool that has the address
+	memPool = memPool.filter(request => request.address !== address);
+	//filter and push new object.
+	memPool.push(newRequest);	
 	console.log(memPool);
 	return newRequest;
 }
@@ -68,16 +73,18 @@ function validateSignature(address, signature) {
 function createResponseForValidSig(addy) {
 	const requestInfo = checkValidation(addy, 'validated');
 	const {address, requestTimeStamp, message, validationWindow} = requestInfo;	
+	const newValWindow = 1800 - validationWindow;
 	let response =  {
 	  "registerStar": true,
 	  "status": {
 		address,
 		requestTimeStamp,
 		message,
-		validationWindow,
-		"messageSignature": "valid"
+		validationWindow: newValWindow,
+		"messageSignature": true
 	  }
 	};
+	validMemPool.push(response);
 	return response;
 }
 
@@ -96,6 +103,38 @@ function checkBytesOfStory(story) {
 		return buf.toString('hex');
 	}
 }
+
+//function to mutate validMemPool and update with most recent valid request
+function updateValidMemPool(request) {
+	
+}
+
+
+//check if 30 min window is over for valid request within validMemPool
+function checkIfVRequestExpired(addy) {
+	//grab request from validMemPool
+	let currentRequest = validMemPool.filter(request => request.status.address === addy);
+	// destruct status then invoke calculateNewValidationWindow(request, timeLimit)
+	const { status } = currentRequest[0];
+	const updatedVRequest = calculateNewValidationWindow(status, 1800000);
+	// if messageSig is missing.. we know time has expired
+	if (!updatedVRequest.messageSignature) {
+		//mutate validMemPool to have most recent updatedVRequest
+		validMemPool = ;
+		return true;
+	}
+	// so return true and remove this request from the validMemPool
+	
+	// 
+	
+	const timeStamp = parseInt(currentRequest[0].status.requestTimeStamp);
+	//1800000
+	if (currentRequest[0].status.) {
+		
+	}
+}
+
+
 
 module.exports = {
 	checkValidation,
