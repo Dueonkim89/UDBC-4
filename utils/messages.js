943e9a849137aa0b270161a2836b66fa5db83ce8
Dueon Kim <dueonkim89@Yahoo.com>
2018-11-18 16:24:18 -0800

    new error message in case user is already validated

--- utils/messages.js
+++ utils/messages.js
@@ -16,6 +16,7 @@ const invalidASCII = 'Your ASCII text contains invalid characters. Please try ag
 const storyIsTooLong = 'Star story can have a maximum length of 250 words or 500 bytes. Please shorten your story.';
 const invalidSignature = 'Your signature is invalid. Please make sure you have the correct message.';
 const validRequestExpired = `Your 30 min window to submit star data has expired. Please obtain a new validation request.`;
+const requestAlreadyValidated = `Your request is already validated. Please submit a star data within your validation window`;
 
 module.exports = {
 	missingInfoForStarPosting,
@@ -23,5 +24,6 @@ module.exports = {
 	invalidSignature,
 	invalidASCII,
 	storyIsTooLong,
-	validRequestExpired
+	validRequestExpired,
+	requestAlreadyValidated
 }
